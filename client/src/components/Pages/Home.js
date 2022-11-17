7c8acefe65dc6934a9e1d0f2ae86bfefc7c308c5
Phinjock Sherpa <phinjock@gmail.com>
2022-11-17 08:55:02 -0800

    worked on placements/framework according to wireframe for home and page 2 testing chakra ui

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,18 +1,39 @@
-import React from "react";
+// import React from "react";
 
-import { Box } from "@chakra-ui/react";
+// import { Box } from "@chakra-ui/react";
 
-const tryOut = new URL("../../images/tryout.png", import.meta.url);
+// };
 
-const Home = () => {
+// export default Home;
+
+import { Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+
+export default function Home() {
   return (
-    <div className="/" id="Home">
-      <h1>Home</h1>
-      <Box boxSize="sm">
-        <img src={tryOut} alt="tryout" />
-      </Box>
-    </div>
+    <Stack className="/" id="Home">
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading
+            fontSize={{ base: "3xl", md: "4xl", lg: "2xl" }}
+            textAlign={"center"}
+          >
+            <Text as={"span"} position={"relative"}>
+              Barcade
+            </Text>
+            <br />{" "}
+            <Text color={"black"} as={"span"}>
+              some good quoutes here
+            </Text>{" "}
+          </Heading>
+        </Stack>
+      </Flex>
+      <Image
+        mt={2}
+        borderRadius={"15"}
+        alt={"Barcade"}
+        objectFit={"cover"}
+        src={"https://pbs.twimg.com/media/EUZbHlbUEAANCyD.jpg:large"}
+      />
+    </Stack>
   );
-};
-
-export default Home;
+}
