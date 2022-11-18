d2fe82856cd4def910f67656833334277089d00e
Cassie Kise <cassandrakise@gmail.com>
2022-11-17 21:32:47 -0800

    chatmessage file creation and added chat component to app.js for import

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Pages/Home";
+import Chat from "./components/Chat/ChatMessage"
 import Barcadians from "./components/Pages/Barcadians";
 import Game from "./components/Pages/Game";
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <Chat /> 
       <Game />
       <Barcadians />
     </div>
