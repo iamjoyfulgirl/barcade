2cd29e02137a9733146bf192578b94d81c0eeba7
Phinjock Sherpa <phinjock@gmail.com>
2022-11-19 14:49:48 -0800

    signin signup functions pushed up, need to fix and update token to be created

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,28 @@ import Footer from "./components/Footer/Footer";
 import Game from "./Pages/Game";
 // import Flappyird from "./components/Pages/Flappybird";
 import "./index.css";
-import Signin from "./components/Access/Signin";
+import Signup from "./components/Access/Signup";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Signin />
-      <Home />
-      {/* <Chat /> */}
-      <Game />
-      {/* <Flappyird /> */}
-      <Barcadians />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Signup />
+        <Home />
+        {/* <Chat /> */}
+        <Game />
+        {/* <Flappyird /> */}
+        <Barcadians />
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,6 +17,7 @@ const resolvers = {
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
+      console.log("user:", user, token);
       return { token, user };
     },
     login: async (parent, { email, password }) => {
