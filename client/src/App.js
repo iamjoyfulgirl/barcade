28cdf41d498efab9521aa1c5864b326eb6ba779f
Phinjock Sherpa <phinjock@gmail.com>
2022-11-19 11:23:10 -0800

    fixed directories and imports updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Pages/Home";
+import Home from "./Pages/Home";
 // import Chat from "./components/Chat/ChatMessage";
-import Barcadians from "./components/Pages/Barcadians";
+import Barcadians from "./Pages/Barcadians";
 import Footer from "./components/Footer/Footer";
-import Game from "./components/Pages/Game";
+import Game from "./Pages/Game";
 // import Flappyird from "./components/Pages/Flappybird";
 import "./index.css";
 import Signin from "./components/Access/Signin";
-
 
 function App() {
   return (
