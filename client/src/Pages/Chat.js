961b1a2183ab9ac6aaf532387ef1354e8953218f
Cassie Kise <cassandrakise@gmail.com>
2022-11-19 16:44:18 -0800

    error full but chat info added

---
+++ client/src/Pages/Chat.js
@@ -0,0 +1,30 @@
+import React from “react”;
+import { Box, Input, FormControl, Button, ButtonGroup, Container, Flex, Text } from “@chakra-ui/react”;
+import { ChevronRightIcon } from “@chakra-ui/icons”;
+function Chat() {
+    const chatWindow = {
+      imageAlt: ‘Rear view of modern home with pool’,
+    }
+    return (
+    <Box d=“flex” justifyContent=“space-between” w=“100%” h=“91.5vh” p=“10px”>
+       <Text fontSize=‘2xl’>Placeholder for chat feed</Text>
+        <Box p=‘6’>
+          <Box display=‘flex’ alignItems=‘baseline’>
+          </Box>
+          <FormControl>
+              <Input
+                placeholder=“Enter Message”
+                mb={3}
+                // onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                rightIcon={<ChevronRightIcon/>}
+                colorScheme=“red”
+                variant=“solid”>
+               </Button>
+            </FormControl>
+        </Box>
+      </Box>
+    )
+  }
+export default Chat;
--- client/src/components/Chat/ChatMessage.js
+++ client/src/components/Chat/ChatMessage.js
@@ -1,8 +1,13 @@
-// import { Button } from "@chakra-ui/react"; // will insert more elements as move through external resources
-import React from "react";
-
+import React from “react”;
+import { Box } from “@chakra-ui/react”;
 const Chat = () => {
-  return <div className="chatArea"></div>;
+    <Box
+        src=“https://img.freepik.com/premium-vector/whatsapp-chat-template_1142-955.jpg?w=826”
+        width=“100%”
+        sx={{
+            aspectRatio: “16/9”,
+        }}
+    />;
 };
-
 export default Chat;
+
