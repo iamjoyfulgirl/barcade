7bb96c065d656a606ae803f3b7707c43e79a1c8f
iamjoyfulgirl <sherri@nymeria.lan>
2022-11-28 22:25:55 -0800

    Add heroku URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/barcade', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/barcade', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
